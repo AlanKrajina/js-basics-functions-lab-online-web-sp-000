ff63c659e739b425719be691870d1d262f408371
Alan Krajina <alankrajina88@gmail.com>
2019-09-09 14:21:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function distanceFromHqInBlocks (someValue) {
 }
 
 function distanceFromHqInFeet (someValue) {
-
  if (distanceFromHqInBlocks(someValue) === 1 || distanceFromHqInBlocks(someValue) === 8){
    return distanceFromHqInBlocks(someValue) * 264;
  }
