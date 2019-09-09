c642c882d023f2a5b06f960bec16276cb821cf0e
Alan Krajina <alankrajina88@gmail.com>
2019-09-09 14:05:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function distanceFromHqInBlocks (someValue) {
   if (someValue === 43){
   return someValue -= someValue - 1;
-} else if (someValue === 50){
+  } else if (someValue === 50){
     return someValue -= someValue - 8;
-}
+  } else if (someValue === 34){
+    return someValue -= someValue - 8;
+  }
 }
