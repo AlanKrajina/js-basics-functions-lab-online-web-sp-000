b1933f63b65bf524e8c3e15a1ceaad9bec6379d4
Alan Krajina <alankrajina88@gmail.com>
2019-09-09 14:48:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,9 +25,13 @@ function distanceTravelledInFeet (someValue1,someValue2) {
 function calculatesFarePrice(start, destination){
   let distance = distanceTravelledInFeet(start,destination)
 
-  if (distance <= 400){
+  if (distance <= 400) {
     return 0;
-  } else if (destination < start){
-    return farePrice = (destination - start)
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * (distance - 400);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
   }
 }
